e1ab859cd68ce9f175a04e475bd0457ea1b888e2
Yasharth kesarwani <devilhunter246800@gmail.com>
2025-01-20 23:42:39 +0530

    Hi

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,52 @@
+let score = "33" 
+console.log(typeof score)
+console.log(typeof (score))
+let valueinInNumber=Number(score)
+console.log(typeof valueinInNumber)
+
+score="22abc"
+valueinInNumber=Number(score)
+console.log(typeof valueinInNumber)
+console.log(valueinInNumber)
+
+score=null
+valueinInNumber=Number(score)
+console.log(typeof valueinInNumber)
+console.log(valueinInNumber)
+
+score=true
+valueinInNumber=Number(score)
+console.log(typeof valueinInNumber)
+console.log(valueinInNumber)
+
+score="yash"
+valueinInNumber=Number(score)
+console.log(typeof valueinInNumber)
+console.log(valueinInNumber)
+
+//"33" => 33
+//"33abc" =>NaN
+// //true => 1 : false => 0
+
+let isLoggedIn=1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+isLoggedIn=""
+booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+isLoggedIn="Yash"
+booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 : true    
+// 2 : false
+// "" : false
+// "Yash" : true
+
+
+let someNumber = 33
+let stringSomeNumber = String(someNumber)
+console.log(stringSomeNumber)
+console.log(typeof stringSomeNumber)
