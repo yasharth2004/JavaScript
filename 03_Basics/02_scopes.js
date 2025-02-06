6730c685b6417ebfc87d79350e82449a086cab87
Yasharth kesarwani <devilhunter246800@gmail.com>
2025-02-06 17:22:42 +0530

    commit 13

---
+++ 03_Basics/02_scopes.js
@@ -0,0 +1,37 @@
+if (true)
+{
+    let a = 10
+    const b = 20
+    var c = 30
+}
+//console.log(a);  Error
+//console.log(b);  Error
+console.log(c);
+
+
+function one()
+{
+    const username= 'Yasharth'
+    function two()
+    {
+        const website = 'youtube'
+        console.log(username);
+         
+    }
+    // console.log(website);
+
+    two()
+    
+}
+one()
+
+if(true)
+{
+    const username= "yasharth"
+    if(username==="yasharth")
+    {
+        const website = " @twitter"
+        console.log(username+website);
+        
+    }
+}
