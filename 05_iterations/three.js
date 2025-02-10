1bb95b1775ce12d980acf4e5590e13136145b7ba
Yasharth kesarwani <devilhunter246800@gmail.com>
2025-02-10 21:14:08 +0530

    15

---
+++ 05_iterations/three.js
@@ -0,0 +1,12 @@
+//for of 
+
+const arr=[1,2,3,4,5]
+for (const num of arr) {
+    console.log(num);
+       
+}
+
+const greeting="Hello World"
+for (const greet of greeting) {
+    console.log(`Each char is ${greet}`);
+}
