fe24aa7e7b0fd3ef24f55bc77f75a22e616356cc
Yasharth kesarwani <devilhunter246800@gmail.com>
2025-02-10 18:38:26 +0530

    14

---
+++ 05_iterations/two.js
@@ -0,0 +1,19 @@
+let index = 0
+while(index<=10)
+{
+    console.log(`Value of index is ${index}`);
+    index++
+}
+
+let myArray =[ 'flash' ,'superman' ,'batman']
+let arr=0;
+while(arr<myArray.length){
+    console.log(myArray [arr]);
+    arr++ 
+}
+
+let score =1
+do{
+    console.log(`Score is ${score}`);
+    score++
+}while (score<=10);
