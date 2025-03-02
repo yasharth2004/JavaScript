9d935ebef1dcd76681c429f560b055b5ca3d4db7
Yasharth kesarwani <devilhunter246800@gmail.com>
2025-03-02 16:50:16 +0530

    commit 26

---
+++ 07_Projects/project3/script.js
@@ -0,0 +1,7 @@
+document.getElementById('clock')
+//document.querySelector('#clock')
+
+setInterval(()=>{let date = new Date ();
+    //console.log(date.toLocaleTimeString());
+    clock.innerHTML=date.toLocaleTimeString();
+},1000)
